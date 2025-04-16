d0e2d3001efb0a49b7dd3226e858f1689643b1a2
Kyle Baraka <kylesamma7@gmail.com>
2025-04-16 02:09:59 -0500

    currently implementing a way to add sprites

--- src/project.py
+++ src/project.py
@@ -1,5 +1,24 @@
 import pygame
 import random
 import os
-import pathlib import Path
 from constants import *
+
+class Monsoons:
+    def __init__(self, name, element, stats, moves):
+        self.name = name
+        self.element = element
+        self.stats = stats
+        self.moves = [Move(move) for move in moves]
+        self.load_sprites()
+
+# This is where the main Monsoon Front and Back sprites are loaded
+    def load_sprites(self):
+        
+
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_HEIGHT))
+    pygame.display.set_caption("Monsoon Rumble")
+    clock - pygame.time.Clock()
