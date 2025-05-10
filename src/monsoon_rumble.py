449a6c06eb873fd7d4969167af51cdb3269564f6
Kyle Baraka <kylesamma7@gmail.com>
2025-05-10 15:41:44 -0500

    Added back Type effectivness

--- src/monsoon_rumble.py
+++ src/monsoon_rumble.py
@@ -1,8 +1,7 @@
 import pygame
 import random
 
-
-
+# Constants
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 FPS = 60
 BG_COLOR = (245, 245, 245)
@@ -10,8 +9,18 @@ WHITE = (255, 255, 255)
 SPRITE_SCALE_FACTOR = 3
 PLAYER_SPRITE_POS = (100, 250)
 OPPONENT_SPRITE_POS = (500, 50)
+MOVE_PANEL_COLOR = (220, 220, 220)
+TEXT_BOX_COLOR = (240, 240, 240)
 
-    pygame.quit()
+# Type chart
+TYPES = ["Fire", "Water", "Plant", "Normal", "Wind", "Electric", "Psychic", "Earth"]
+TYPE_EFFECTIVENESS = {t: {op: 1.0 for op in TYPES} for t in TYPES}
 
-if __name__ == "__main__":
-    main()
+TYPE_EFFECTIVENESS["Fire"].update({"Plant": 2.0, "Normal": 2.0, "Earth": 0.5, "Wind": 0.5})
+TYPE_EFFECTIVENESS["Water"].update({"Fire": 2.0, "Earth": 2.0, "Plant": 0.5, "Electric": 0.5})
+TYPE_EFFECTIVENESS["Plant"].update({"Water": 2.0, "Earth": 2.0, "Fire": 0.5, "Wind": 0.5})
+TYPE_EFFECTIVENESS["Wind"].update({"Fire": 2.0, "Plant": 2.0, "Earth": 0.5})
+TYPE_EFFECTIVENESS["Electric"].update({"Water": 2.0, "Wind": 2.0, "Earth": 0.5})
+TYPE_EFFECTIVENESS["Psychic"].update({"Psychic": 0.5})
+TYPE_EFFECTIVENESS["Earth"].update({"Fire": 2.0, "Electric": 2.0, "Water": 0.5, "Plant": 0.5})
+TYPE_EFFECTIVENESS["Normal"].update({t: 1.0 for t in TYPES})
